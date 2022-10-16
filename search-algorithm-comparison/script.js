aa830731c7146a370f9f12b9e06aad3e0657b4b1
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-16 18:47:31 -0400

    Writing functions for 'linearSearch' and 'binarySearch'

---
+++ search-algorithm-comparison/script.js
@@ -0,0 +1,22 @@
+function linearSearch(array, target) {
+  return array.findIndex((element) => element === target);
+}
+
+function binarySearch(list, target) {
+  let low = 0;
+  let high = list.length - 1;
+
+  while (low <= high) {
+    let mid = (low + high) / 2;
+
+    if (list[mid] === target) {
+      return mid;
+    } else if (list[mid] > target) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return -1;
+}
