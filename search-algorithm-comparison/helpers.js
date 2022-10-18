8c104e96375b5230bfb80a6b1c34aa5d25377769
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-18 14:58:13 -0400

    Removing unnecessary stuff

--- search-algorithm-comparison/helpers.js
+++
@@ -1,6 +0,0 @@
-function sortArray(array) {
-  // ... is known as the spread syntax to copy an array. Not copying the array when sorting will mutate the original array, which we don't want because it can cause unintended side effects
-  return [...array].sort((a, b) => a-b);
-}
-
-module.exports = sortArray
