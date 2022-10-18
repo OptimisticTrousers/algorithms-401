12ded9e169c7b955ffc4d888e229d9b11dab5b4d
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-18 12:13:21 -0400

    Writing helper functions

--- search-algorithm-comparison/script.js
+++ search-algorithm-comparison/script.js
@@ -5,6 +5,34 @@ const binaryDisplay = document.querySelector(".main__binary");
 const arrayDisplay = document.querySelector(".main__display");
 const linearIndexDisplay = document.querySelector(".main__linear-index");
 const binaryIndexDisplay = document.querySelector(".main__binary-index");
+function displayArray(array) {
+  // Displays the array on screen for now
+  arrayDisplay.textContent = array;
+}
+
+// Generates random array
+function generateArray(length) {
+  // Using a set to generate unique, random values for an array
+  const set = new Set();
+  while (set.size !== length) {
+    set.add(Math.floor(Math.random() * 1000000) + 1);
+  }
+
+  // Using Array.from converts this set into an array
+  return Array.from(set);
+}
+
+// Finds the time of any searching algorithm by passing the function reference as 'callback', the array, and target
+function findTime(callback, array, target) {
+  const timeStart = performance.now();
+  const foundIndex = callback(array, target);
+  const timeEnd = performance.now();
+  const timeTaken = timeEnd - timeStart;
+
+  // Shorter sytanx for if statement
+  return { foundIndex, timeTaken };
+}
+
 function linearSearch(array, target) {
   return array.findIndex((element) => element === target);
 }
