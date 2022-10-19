fdaf7dbe21f5689b3b3ef237064801b84a2668c7
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2022-10-19 19:45:46 -0400

    Adding finishing touches

--- search-algorithm-comparison/script.js
+++ search-algorithm-comparison/script.js
@@ -4,47 +4,54 @@ const linearSpeedDisplay = document.querySelectorAll(".main__speed")[0];
 const binarySpeedDisplay = document.querySelectorAll(".main__speed")[1];
 const arrayDisplay = document.querySelector(".main__array");
 const arrayLength = document.querySelector(".main__length");
-const linearIndexDisplay = document.querySelectorAll(".main__index")[0];
-const binaryIndexDisplay = document.querySelectorAll(".main__index")[1];
+
+const foundElementDisplay = document.querySelector(".main__found");
+
+const arrayLengthInput = document.querySelector(".main__input");
 
 // Event listener called 'onClick' for click event on button
 mainButton.addEventListener("click", onClick);
 
 // Function that occurs when the button is clicked
 function onClick() {
-  // Creating a random array of unique values that is size 1,000,000
-  const randomArray = generateArray(1000000);
+  // Creating a random array of unique values that is size 1,000,000 or the user generated value
+
+  const randomArray = generateArray(parseInt(arrayLengthInput.value));
   // Sorting the array for binary search
   const sortedRandomArray = sortArray(randomArray);
+
+  const randomIndex = Math.floor(Math.random() * randomArray.length);
+
+  const randomTarget = sortedRandomArray[randomIndex];
   displayArray(randomArray);
   // Arbitrarily picking 5 as the target
   // Don't worry about this part if it's confusing, but just know it gets the results from the 'findTime' helper function
-  const { foundIndex: binaryIndex, timeTaken: binaryTime } = findTime(
+  const binarySearchTime = findTime(
     binarySearch,
     sortedRandomArray,
-    5
+    randomTarget
   );
-  const { foundIndex: linearIndex, timeTaken: linearTime } = findTime(
+  const linearSearchTime = findTime(
     linearSearch,
-    randomArray,
-    5
+    sortedRandomArray,
+    randomTarget
   );
   // Changing the text content of these 4 HTML elements
-  linearSpeedDisplay.textContent = `Speed display: ${linearTime} ms`;
-  binarySpeedDisplay.textContent = `Speed display: ${binaryTime} ms`;
-  linearIndexDisplay.textContent = `Found index: ${linearIndex}`;
-  binaryIndexDisplay.textContent = `Found index: ${binaryIndex}`;
+  linearSpeedDisplay.textContent = `Speed: ${linearSearchTime} ms`;
+  binarySpeedDisplay.textContent = `Speed: ${binarySearchTime} ms`;
+  foundElementDisplay.textContent = `Found element: ${sortedRandomArray[randomIndex]}`;
 }
 
 // Generates random array
 function generateArray(length) {
   // Using a set to generate unique, random values for an array
-  const arr = new Array(length);
-  for (let i = 0; i < length; i++) {
-    arr[i] = Math.floor(Math.random() * 100000) + 1;
+  const set = new Set();
+  while (set.size !== length) {
+    set.add(Math.floor(Math.random() * 1000000) + 1);
   }
+
   // Using Array.from converts this set into an array
-  return arr;
+  return Array.from(set);
 }
 
 function sortArray(array) {
@@ -63,12 +70,12 @@ function displayArray(array) {
 // Finds the time of any searching algorithm by passing the function reference as 'callback', the array, and target
 function findTime(callback, array, target) {
   const timeStart = performance.now();
-  const foundIndex = callback(array, target);
+  callback(array, target);
   const timeEnd = performance.now();
-  const timeTaken = (timeEnd - timeStart).toFixed(4);
+  const timeTaken = timeEnd - timeStart;
 
-  // Returning an object with the foundIndex and timeTaken
-  return { foundIndex, timeTaken };
+  // Returning timeTaken
+  return timeTaken;
 }
 
 // Built in JS function to search for index. O(n) time
