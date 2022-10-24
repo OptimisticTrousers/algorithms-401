51fa19139427f258da2a2c2103a1f315797b36c5
BuildTools <unconfigured@null.spigotmc.org>
2022-10-23 20:14:48 -0400

    adding binomial coefficients file

---
+++ binomialcoefficients.java
@@ -0,0 +1,17 @@
+public class binomialcoefficients {
+    public static void main(String[] args){
+        
+    }
+    
+    public static void getBinomialCoefficients(int nth){
+
+    }   
+
+    /*
+    1
+    1 1
+    1 2 1
+    1 3 3 1
+    1 4 6 4 1
+    */
+}
